c5b7b14a54175bdbce95e72bcffeb11217d16b26
Mariami <kochadzemariam80@gmail.com>
2023-02-25 05:11:25 -0800

    This is the first version of my cool application code

---
+++ index.js
@@ -0,0 +1,18 @@
+const user1 = {
+  name: "mariam",
+  lastname: "kochadze",
+};
+
+const user2 = {
+  name: "tornike",
+  lastname: "machitidze",
+};
+
+const user3 = {
+  name: "jay",
+  lastname: "kay",
+};
+
+const users = [user1, user2, user3];
+
+console.log(users[1]);
